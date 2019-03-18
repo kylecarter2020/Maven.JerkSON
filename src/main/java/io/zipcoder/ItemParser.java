60efe4dc7924c9d4cf46df08b85620928f578c0f
Kyle Carter <elyko.tech@gmail.com>
2019-03-18 13:18:09 -0400

    all single item tests passing

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,7 +3,10 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
@@ -11,6 +14,22 @@ public class ItemParser {
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        List<String> matched = new ArrayList<>();
+
+        singleItem = singleItem.toLowerCase();
+
+        Pattern pattern = Pattern.compile("[:|@|^|%](.*?)[;|##]");
+        Matcher matcher = pattern.matcher(singleItem);
+
+        while(matcher.find()) {
+            matched.add(matcher.group(1));
+        }
+        
+        String name = matched.get(0);
+        double price = Double.parseDouble(matched.get(1));
+        String type = matched.get(2);
+        String expiration = matched.get(3);
+
+        return new Item(name, price, type, expiration);
     }
 }
